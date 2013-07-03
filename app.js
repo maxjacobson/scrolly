ba998d242006995642b39b6fde0115ef945d5937
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-03 11:27:54 -0400

    random colors

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+var random_hex = function() {
+  // from http://www.paulirish.com/2009/random-hex-color-code-snippets/
+  var r = '#'+Math.floor(Math.random()*16777215).toString(16);
+  return r;
+};
+
 $(document).ready(function() {
   $("body").addClass("pretty");
   $(document).scroll(function() {
     var position = $(document).scrollTop();
-    if(position > 500) {
-      $("body").toggleClass("ugly");
+    if(position % 5 === 0) {
+      $("body").css("background-color", random_hex());
     }
   });
 });
